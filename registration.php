2956bead74156b2e7f67b9be14734ebe9a756a38
Luke Rodgers <luke.rodgers@gene.co.uk>
2025-07-09 13:23:33 +0100

    Rename module

--- registration.php
+++ registration.php
@@ -2,4 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Gene_NewrelicExtras', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Gene_NewrelicRumPageType', __DIR__);
